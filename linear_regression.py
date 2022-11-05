df4026fd103ff3e8a01fd93a823fb0e36c38f75f
rafaelBauer <abauer.rafael@gmail.com>
2022-11-05 16:06:48 +0100

    Implemented exercise 1

--- linear_regression.py
+++ linear_regression.py
@@ -4,10 +4,16 @@ import matplotlib.pyplot as plt
 
 class LinearRegression:
     def __init__(self):
-        pass
+        self.w = np.array([])
 
-    def run(self):
-        pass
+    def train(self, X, y):
+        x_trans = np.transpose(X)
+        inverse = np.linalg.inv(np.matmul(x_trans, X))
+        self.w = np.matmul(np.matmul(inverse, x_trans), y)
+
+    def run_model(self, x):
+        return np.matmul(x, self.w)
+
 
 
 def plot(data):
@@ -18,9 +24,18 @@ def plot(data):
 
 
 if __name__ == '__main__':
-    ages = np.array([18, 58, 23, 45, 63, 36])
-    BMIs = np.array([53.13, 49.06, 17.38, 21, 21.66, 28.59])
-    charges = np.array([1163.43, 11381.33, 2775, 7222, 14349, 6548])
+    ages = np.array([[18, 58, 23, 45, 63, 36]]).T
+    BMIs = np.array([[53.13, 49.06, 17.38, 21, 21.66, 28.59]]).T
+    charges = np.array([[1163.43, 11381.33, 2775, 7222, 14349, 6548]]).T
+
+    data = np.append(ages, BMIs, axis=1)
+    linear_regression = LinearRegression()
+    linear_regression.train(data, charges)
+
+    newInput = np.array([40, 32.5])
+    newOutput = linear_regression.run_model(newInput)
+    print(newOutput)
+
 
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -28,5 +43,6 @@ if __name__ == '__main__':
     ax.set_ylabel('BMI')
     ax.set_zlabel('Charges')
     ax.scatter(ages, BMIs, charges, marker='o')
+    ax.scatter(newInput[0],newInput[1], newOutput, marker='x')
     # plt.legend()
     plt.show()
